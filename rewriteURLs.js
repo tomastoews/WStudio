f9519ee73d00280f933ace2c29ef30691585d8fe
Tomas Töws <tomas.toews96@gmx.de>
2019-11-14 22:35:27 +0100

    Added url replacement script

---
+++ rewriteURLs.js
@@ -0,0 +1,43 @@
+const replace = require('replace-in-file');
+const path = require('path');
+
+// Website zum Erstellen von Regulären Ausdrücken (RegEx):
+// https://ibnuhx.com/regex-generator/?ref=madewithvuejs.com
+
+const performReplacements = ({files, from, to, countMatches}) => {
+    const options = {files, from, to, countMatches};
+    const results = replace.sync(options);
+    console.log('Replace result: ', results);
+};
+
+// HTML Files
+performReplacements({
+    files: path.join(__dirname, 'dist/*.html'),
+    from: [
+        /href="\//g,
+        /src="\//g,
+        /<base href="/g
+    ],
+    to: [
+        'href="',
+        'src="',
+        '<base href="/',
+    ],
+    countMatches: true,
+});
+
+// CSS Files
+performReplacements({
+    files: path.join(__dirname, 'dist/*.css'),
+    from: [
+        /url\(\//g,
+        /url\(\'\//g,
+        /url\("\//g
+    ],
+    to: [
+        'url(',
+        'url(\'',
+        'url("/'
+    ],
+    countMatches: true,
+});
